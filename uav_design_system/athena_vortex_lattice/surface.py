c2b5adb80cbb78d424d25b850d2ebf9aa7ffb9b5
Vikash Tank <vikashtank@hotmail.co.uk>
2017-11-23 09:57:24 +0000

    add documentation to surface file

--- uav_design_system/athena_vortex_lattice/surface.py
+++ uav_design_system/athena_vortex_lattice/surface.py
@@ -23,22 +23,44 @@ class Surface(collections.UserList):
         self.define_mesh()
         self.define_translation_bias()
 
-    def append(self, value):
-        self._sections.append(value)
-
     def define_mesh(self, number_cord: int = 20, number_span: int = 40,
                     cord_distribution: float = 1, span_distribution: float = 1):
+        """
+        define the number of grid lines and their distribution on the surface.
+
+        inputs:
+            number_cord: number of grid lines the cord is divided into
+            number_span: number of grid lines the span is divided into
+            cord_distribution: A number from 3 to -3, 3 being equal spacing
+                                2 being sinusoidal spacing
+                                1 being cosine spacing
+                                Please see documentation for athena vortex lattice
+                                for more information
+            span_distribution: The same as cord distribution but along the span
+        """
         self.number_cord = number_cord
         self.number_span = number_span
         self.cord_distribution = cord_distribution
         self.span_distribution = span_distribution
 
     def define_translation_bias(self, x: float = 0, y: float = 0, z: float = 0):
+        """
+        define the location of the origin of the wing compared to the global
+        origin. defaulted to the global origin
+
+        inputs:
+            x: location downstream, along velocity vector
+            y: location along span.
+            z: location upwards.
+        """
         self.x = x
         self.y = y
         self.z = z
 
     def add_section(self, section: 'Section'):
+        """
+        add a section to the surface
+        """
         self.sections.append(section)
 
     def __iter__(self):
@@ -74,7 +96,7 @@ class Section():
         pass
 
     def add_aerofoil(self, aerofoil_file_path: str):
-        self.aerofoil = aerofoil
+        self.aerofoil = aerofoil_file_path
 
     def _add_control_surface(self, control_surface: 'ControlSurface'):
         self.control_surface = control_surface
@@ -88,6 +110,16 @@ class Section():
 
 
 class ControlSurface():
+    """
+    a control surface class
+
+    inputs:
+        name: give a name to the control surface for identification
+        xhinge: the percentage of the cord this surface will take up
+        deflection_type: the type of control surface: flaps or elevators
+        gain: for the calculations of derivatives, this can be tuned to
+              how much the surface moves in response to inputs
+    """
 
     def __init__(self, name: str, xhinge: float,
                        rotation_axis: List[float],
