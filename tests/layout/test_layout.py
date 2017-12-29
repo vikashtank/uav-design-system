7b1f08a256344e3ef472c51432991e1ebfecca73
Vikash Tank <vikashtank@hotmail.co.uk>
2017-12-29 13:01:39 +0000

    set up layout class

---
+++ tests/layout/test_layout.py
@@ -0,0 +1,7 @@
+from os.path import join, exists, dirname, abspath
+from os import makedirs
+this_directory = dirname(abspath(__file__))
+import sys
+sys.path.append(this_directory + "/../../")  # so uggo thanks to atom runner
+import unittest
+import uav_design_system.layout as layout
