bb7d47ad92d86419d95960f92ed50773b4d4de9f
Vikash Tank <vikashtank@hotmail.co.uk>
2017-12-29 12:47:48 +0000

    add test to check file content against manual run results

--- tests/athena_vortex_lattice/test_run.py
+++ tests/athena_vortex_lattice/test_run.py
@@ -7,9 +7,24 @@ import unittest
 from uav_design_system.athena_vortex_lattice import AVLRunner
 import shutil
 
+class CustomAssertions():
 
-class Test(unittest.TestCase):
+    def _read_file(self, file):
+        with open(file) as open_file:
+            return open_file.read()
 
+    def assertFilesSame(self, file1: str, file2: str):
+        """
+        tests that the content of two files are the same
+        """
+        file1_content = self._read_file(file1)
+        file2_content = self._read_file(file2)
+
+        if file1_content != file2_content:
+            raise AssertionError("{0} is not the same as {1}".format(file1,
+                                                                     file2))
+
+class TestRun(unittest.TestCase, CustomAssertions):
 
     def setUp(self):
         self.results_dir = join(this_directory, "results_dir")
@@ -17,26 +32,57 @@ class Test(unittest.TestCase):
             shutil.rmtree(self.results_dir)
         except FileNotFoundError:
             pass
+        makedirs(self.results_dir)
+
+        self.resources_folder = join(this_directory, "resources")
+        self.geom_file = join(self.resources_folder, "allegro.avl")
+        self.mass_file = join(self.resources_folder, "allegro.mass")
+        self.config_file = join(self.resources_folder, "bd2.run")
 
     def tearDown(self):
         shutil.rmtree(self.results_dir)
 
-    def test_run_success(self):
-
-        geom_file = join(this_directory, "resources", "allegro.avl")
-        mass_file = join(this_directory, "resources", "allegro.mass")
-        config_file = join(this_directory, "resources", "bd2.run")
-
-        makedirs(self.results_dir)
+    def test_run_create_files(self):
+        """
+        uses files in resources to test if avl is generating the correct files
+        """
 
         avl_runner = AVLRunner()
-        avl_runner.setup_analysis( geom_file, mass_file, config_file)
+        avl_runner.setup_analysis(self.geom_file,
+                                  self.mass_file,
+                                  self.config_file)
         results_dict = avl_runner.generate_results(self.results_dir)
 
         self.assertTrue(exists(join(self.results_dir, "ft.txt")))
         self.assertTrue(exists(join(self.results_dir, "hm.txt")))
         self.assertTrue(exists(join(self.results_dir, "st.txt")))
 
+    def test_run_correct_file_content(self):
+        """
+        checks the files created by AVL running contain the correct content,
+        when compared to the manual runs
+        """
+        avl_runner = AVLRunner()
+        avl_runner.setup_analysis(self.geom_file,
+                                  self.mass_file,
+                                  self.config_file)
+        avl_runner.generate_results(self.results_dir)
+        results_dict = avl_runner.generate_results("/Users/VikashTank/Desktop")
+
+        # get expected files
+        expected_ft = join(self.resources_folder, "ft.txt")
+        expected_hm = join(self.resources_folder, "hm.txt")
+        expected_st = join(self.resources_folder, "st.txt")
+
+        # get actual files
+        actual_ft = join(self.results_dir, "ft.txt")
+        actual_hm = join(self.results_dir, "hm.txt")
+        actual_st = join(self.results_dir, "st.txt")
+
+        # check files are the same
+        self.assertFilesSame(expected_ft, actual_ft)
+        self.assertFilesSame(expected_hm, actual_hm)
+        self.assertFilesSame(expected_st, actual_st)
 
 
 
--- uav_design_system/athena_vortex_lattice/run.py
+++ uav_design_system/athena_vortex_lattice/run.py
@@ -12,8 +12,8 @@ from process import Process, Runner
 class AVLRunner(Runner):
 
     result_aliases = {"st": "stability derivatives",
-                        "ft" : "forces",
-                        "hm" : "hinge moments"}
+                      "ft" : "forces",
+                      "hm" : "hinge moments"}
 
     def __init__(self):
         super().__init__(os.path.join(os.path.dirname(__file__), "avl3.35"))
@@ -29,17 +29,20 @@ class AVLRunner(Runner):
             config_file: configuration file path for AVL
             required_files: additional file paths for files used in analysis
         """
+        # move files to run time directory
         self.geom_file = self._move_to_runtime(geom_file)
         self.mass_file = self._move_to_runtime(mass_file)
         self.config_file = self._move_to_runtime(config_file)
         for required_file in required_files:
             self._move_to_runtime(required_file)
+
+        # begin avl executable
         self.process = Process.initialise_process(self.executable)
 
         self.process.command("LOAD " + self.geom_file)
+        self.process.command("CASE " + self.config_file)
         self.process.command("MASS " + self.mass_file)
         self.process.command("MSET 0")
-        self.process.command("OPER " + "f " + self.config_file)
 
 
     def generate_results(self, results_dir: str = ""):
@@ -56,15 +59,20 @@ class AVLRunner(Runner):
             format
         """
         results_dict = {}
+        self.process.command("OPER")
+        self.process.command("c1")
+        self.process.command("")
         self.process.command("X")
 
         for analysis_name in AVLRunner.result_aliases.keys():
 
-            temp_file = analysis_name + ".txt"
+            temp_file = os.path.join(self.temp_folder, analysis_name + ".txt")
             content = self._get_results(analysis_name, temp_file)
+
             analysis_alias = AVLRunner.result_aliases[analysis_name]
             results_dict[analysis_alias] = content
             shutil.copy(temp_file, results_dir)
+
 
         return results_dict
 
@@ -82,6 +90,9 @@ class AVLRunner(Runner):
         """
         command = analysis_name + " {0}".format(file_name)
         self.process.command(command)
+
+        # BUG: when the print_it is performed here the values of xref and zref
+        # are different to those put in output filex
 
         while not os.path.exists(file_name):
             time.sleep(0.1)
