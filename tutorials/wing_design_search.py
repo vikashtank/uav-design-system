f6573010c7e2639676cfe0a99081e8abad7a5763
Vikash Tank <vikashtank@hotmail.co.uk>
2018-01-02 12:08:34 +0000

    fix tutorial to take correct aerofoil file

--- tutorials/wing_design_search.py
+++ tutorials/wing_design_search.py
@@ -12,19 +12,19 @@ def create_wing(file_name):
     surface.define_mesh(20, 30, 1.0, 1.0)
 
     cord1 = 0.8
-    section1 = avl.Section("aerofoil1.txt", cord1)
+    section1 = avl.Section("aerofoil.txt", cord1)
 
     cord2 = 0.3
-    section2 = avl.Section("aerofoil2.txt", cord2)
+    section2 = avl.Section("aerofoil.txt", cord2)
     section2.translation_bias(cord1  - cord2, 0.3, 0)
 
     cord3 = 0.05
-    section3 = avl.Section("aerofoil3.txt", cord3)
+    section3 = avl.Section("aerofoil.txt", cord3)
     section3.translation_bias(cord1 - cord3, 0.85, 0)
 
-    control_surface = avl.ControlSurface("elevator", 0.6, [0, 1, 0], avl.ControlDeflectionType.SYMMETRIC)
+    control_surface = avl.ControlSurface("elevator", 0.3, [0, 1, 0], avl.ControlDeflectionType.SYMMETRIC)
     surface.add_section(section1, section2, section3)
-    surface.add_control_surface(control_surface, 0, 2)
+    surface.add_control_surface(control_surface, 1, 2)
 
     with open(file_name, "w") as open_file:
         open_file.write(str(surface))
@@ -45,7 +45,8 @@ def run_analysis(geom_file, mass_file, config_file, results_dir):
 
 if __name__ == "__main__":
 
-    shutil.rmtree(join(this_directory, "results2"))
+    results_dir = "/Users/VikashTank/Desktop/athena_results"
+    makedirs(results_dir)
 
     pho = 1.225
     vel = 30
@@ -53,19 +54,21 @@ if __name__ == "__main__":
     m = 6.8
     w = m*g
 
-
-    avl_file_name = join(this_directory, "test.avl")
+    # create
+    avl_file_name = join(results_dir, "test.avl")
     wing = create_wing(avl_file_name)
+
+
     surface_area = wing.area
     cl_trim = w/(0.5*vel*vel*pho*surface_area)
-    run_file_name = join(this_directory, "test.run")
+
+    # create run file
+    run_file_name = join(results_dir, "test.run")
+    print(run_file_name)
     with open( run_file_name, "w") as open_file:
         open_file.write(avl.create_run_file(6.8, 30, 0.5, cl_trim))
+
     #create dummy mass file
-    mass_file_name = join(this_directory, "mass.mass")
+    mass_file_name = join(results_dir, "mass.mass")
     with open( mass_file_name, "w") as open_file:
         open_file.write("")
-
-    results_dir = join(this_directory, "results2")
-
-    run_analysis(avl_file_name, mass_file_name, run_file_name, results_dir)
