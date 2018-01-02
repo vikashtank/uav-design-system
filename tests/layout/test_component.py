b9cec2b8cfce8a73f6dd54facdd0563c631be984
Vikash Tank <vikashtank@hotmail.co.uk>
2018-01-02 15:28:39 +0000

    add method to create a foam wing model from a surface class with mass and center of gravity properties

---
+++ tests/layout/test_component.py
@@ -0,0 +1,53 @@
+from os.path import join, exists, dirname, abspath
+from os import makedirs
+this_directory = dirname(abspath(__file__))
+import sys
+sys.path.append(this_directory + "/../../")  # so uggo thanks to atom runner
+import unittest
+from uav_design_system import layout, athena_vortex_lattice as avl
+
+
+class TestCreateFoamWing(unittest.TestCase):
+    """
+    tests the create foam wing method in the StructureFactory class
+    """
+
+    def setUp(self):
+        surface = avl.Surface(name = "test_surface")
+
+        for i in range(5):
+            section = avl.Section("", 1)
+            section.translation_bias(0, i, 0)
+            surface.add_section(section)
+
+        foam_factory = layout.StructureFactory(layout.StructuralModelType.HOLLOWFOAM)
+        self.structural_model = foam_factory(surface, wall_thickness = 1)
+
+    def tearDown(self):
+        pass
+
+
+    def test_structural_factory_foam(self):
+        """
+        tests that the stuctural factory produces the correct structural model
+        for the foam model and returns the correct centers of gravity
+        """
+        self.assertTrue(isinstance(self.structural_model, layout.StructuralModel))
+        self.assertEqual(len(self.structural_model.objects), 4)
+
+        for index, section in enumerate(self.structural_model.objects):
+            point = layout.Point(0.5, index + 0.5, 1)
+            self.assertEqual(section.center_of_gravity_global.x, point.x)
+            self.assertEqual(section.center_of_gravity_global.y, point.y)
+            self.assertEqual(section.center_of_gravity_global.z, point.z)
+
+    def test_total_mass(self):
+        density = 5
+        thickness = 1
+        number_of_sections = 4
+        self.assertEqual(self.structural_model.total_mass, density * 2 *
+                         thickness * number_of_sections)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ uav_design_system/layout/component.py
@@ -0,0 +1,82 @@
+"""
+takes geometry and mass objects and creates componentns such as beams and
+structures
+"""
+from enum import Enum
+from os.path import dirname, abspath
+this_directory = dirname(abspath(__file__))
+import sys
+sys.path.append(this_directory)# so uggo thanks to atom runner
+import layout
+import geometry
+
+
+class StructuralModelType(Enum):
+    """
+    Used for selection of the structural model to be applied to wing
+    """
+    HOLLOWFOAM = 1
+
+class StructuralModel(layout.Arrangement):
+
+    def __init__(self, name = "", *objects):
+        super().__init__(name, *objects)
+
+
+
+class FoamSection(layout.MassObject):
+
+    FOAMDENSITY = 5
+
+    def __init__(self, x1: float, x2: float, x_shift: float, y: float,
+                 thickness: float, name: str = ""):
+        trapezium_plate = geometry.TrapeziumPlate(x1, x2, x_shift, y, thickness)
+        super().__init__(trapezium_plate, FoamSection.FOAMDENSITY, name)
+
+
+class StructureFactory():
+    """
+    builds wing structures from surface classes
+    """
+
+    def __init__(self, model: StructuralModel):
+        self.model = model
+
+    def __call__(self, surface: "Surface", *args, **kwargs):
+        if self.model == StructuralModelType.HOLLOWFOAM:
+            structure = self._create_foam_wing(surface, *args, **kwargs)
+
+        return structure
+
+    def _create_foam_wing(self, surface: "Surface", wall_thickness: float):
+        """
+        takes a surface class and applies a structure model, returning a wing
+        structure class
+        """
+
+        foam_model = StructuralModel(surface.name)
+
+        # loop through pairs of sections and create foam sections
+        for index in range(len(surface) - 1):
+            section1 = surface.sections[index]
+            section2 = surface.sections[index + 1]
+
+            name = "Section {0}".format(index)
+            x1 = section1.cord
+            x2 = section2.cord
+            y = section2.y - section1.y
+            x_shift = section1.x - section2.x
+            thickness = wall_thickness * 2
+
+            section = FoamSection(x1, x2, x_shift, y, thickness, name)
+            section.location = geometry.Point(section1.x,
+                                                section1.y,
+                                                section1.z)
+
+            foam_model.append(section)
+
+        return foam_model
+
+
+if __name__ == "__main__":
+    pass
