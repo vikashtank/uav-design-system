666ced374762c022226b178311f6f065739b49db
Vikash Tank <vikashtank@hotmail.co.uk>
2018-01-11 14:40:28 +0000

    add test for foam section location

--- tests/layout/test_component.py
+++ tests/layout/test_component.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(this_directory + "/../../")  # so uggo thanks to atom runner
 import unittest
 from uav_design_system import layout, athena_vortex_lattice as avl
-
+from matplotlib import pyplot as plt
 
 class TestCreateFoamWing(unittest.TestCase):
     """
@@ -17,7 +17,7 @@ class TestCreateFoamWing(unittest.TestCase):
 
         # add five square sections side by side to form a rectangular surface
         for i in range(5):
-            section = avl.Section("", 1)
+            section = avl.Section(1)
             section.translation_bias(0, i, 0)
             surface.add_section(section)
 
@@ -48,6 +48,48 @@ class TestCreateFoamWing(unittest.TestCase):
         number_of_sections = 4
         self.assertEqual(self.structural_model.total_mass, density * 2 *
                          thickness * number_of_sections)
+
+
+class TestCreateFoamWingTrapezium(unittest.TestCase):
+
+    def setUp(self):
+        self.surface = avl.Surface(name = "test_surface")
+
+        # add five square sections side by side to form a rectangular surface
+        for i in range(5):
+            section = avl.Section(1)
+            section.translation_bias(-i, i, 0)
+            self.surface.add_section(section)
+
+        foam_factory = layout.StructureFactory(layout.StructuralModelType.HOLLOWFOAM)
+        self.structural_model = foam_factory(self.surface, wall_thickness = 1)
+
+    def _test_plot(self):
+        """
+        test the plots of the structural model overlap the plots of the surface
+        """
+        for mass in self.structural_model.flatten().all_mass_objects:
+            x, y = mass.geometry.project_xy.plot_coordinates
+            plt.plot(x, y)
+            cg_x = mass.center_of_gravity_global.x
+            cg_y = mass.center_of_gravity_global.y
+            plt.plot(cg_x, cg_y, "*")
+
+        x, y, _ = self.surface.get_plot_coordinates()
+        plt.plot(x, y, "r--")
+        plt.show()
+
+class TestFoamSection(unittest.TestCase):
+
+    def setUp(self):
+        self.section = layout.FoamSection(10, 5, 2.5, 10, 1)
+
+    def test_location_default(self):
+        self.assertEqual(self.section.location, layout.Point(0, 0, 0))
+
+    def test_location_set(self):
+        self.section.location = layout.Point(1, 7, -3)
+        self.assertEqual(self.section.location, layout.Point(1, 7, -3))
 
 
 if __name__ == "__main__":
