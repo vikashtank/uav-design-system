2913342e1c94debd08508a9b22bb4811a26a32f4
Vikash Tank <vikashtank@hotmail.co.uk>
2018-01-27 19:25:09 +0000

    add method to create random dictionary
    add a method to the genetic class to create a random dictionary from the
    schema

--- tests/optimisation/test_genetic.py
+++ tests/optimisation/test_genetic.py
@@ -9,10 +9,29 @@ import uav_design_system.optimisation as opt
 class TestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.input_dict = {
+                            "name1": {
+                                        "max": 5,
+                                        "min": 4
+                                     },
+                            "name2": {
+                                        "max": 7,
+                                        "min": 2
+                                     }
+                           }
+        self.schema = opt.Schema.from_dict(self.input_dict)
+        self.Genetic = opt.Genetic(opt.GeneticFactory(), self.schema)
 
-    def test_(self):
-        pass
+    def test_create_random_dict(self):
+        """
+        tests a random dict is created within schema constraints
+        """
+        actual_dict = self.Genetic._create_random_dict()
+        self.assertTrue("name1" in actual_dict)
+        self.assertTrue("name2" in actual_dict)
+        self.assertTrue(4 < actual_dict["name1"] < 5)
+        self.assertTrue(2 < actual_dict["name2"] < 7)
+
 
 
 if __name__ == "__main__":
--- uav_design_system/optimisation/genetic.py
+++ uav_design_system/optimisation/genetic.py
@@ -2,21 +2,46 @@
 file for construting the genetic algorithm for use in this project
 """
 from abc import ABC, abstractmethod
+from .schema import Schema
+import random
+
+
+class Child():
+
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.child = None
+        self.results = None
+
+
+class GeneticFactory(ABC):
+    pass
+
 
 class Genetic(ABC):
 
 
-    def __init__(self, factory, factory_args, ):
-        self.population_size = None
+    def __init__(self, factory: GeneticFactory, schema: Schema):
+        self.factory = factory
+        self.schema = schema
 
-    def _fitness(self):
+    def fitness(self):
         pass
 
-    def generate_first_population(self, population_size):
+    def generate_population(self, population_size):
         """
         generate many potential designs for the initial run
         """
-        self.population_size = population_size
+        population = []
+
+        while(len(population) < population_size):
+            child = create_random_child()
+            if self.is_valid_child(child):
+                population.append(child)
+
+        return population
+
+    def analyse(self, child: Child):
         pass
 
     def mutate(self):
@@ -25,12 +50,34 @@ class Genetic(ABC):
         """
         pass
 
-    @abstractmethod
-    def create_child(self):
-        pass
+    def run_genetic(self, generations = 10, store = False, display = False):
 
-    def create_children(self):
-        pass
+        for child in generate_population():
+            child.results = self.analyse(child)
+            fitness = self.fitness(child.results)
+
+    def combine(self, child1: Child, child2: Child):
+        """
+        takes two children, and combines their input dictionaries to create
+        a new child
+        """
+        child3_inputs = child1.inputs + child2.inputs
+        return Child(child3)
+
+    def create_random_child(self, kwargs):
+        # create a child dictionary from schema
+        new_child_dict = self._create_random_dict()
+
+    def _create_random_dict(self):
+        """
+        creates a random dictionary within the schema constraints
+        """
+        new_child_dict = {}
+        for constraint in self.schema:
+            val = random.uniform(constraint.min, constraint.max)
+            val = round(val, 4)
+            new_child_dict[constraint.name] = val
+        return new_child_dict
 
 
 class GeneticInterface(ABC):
