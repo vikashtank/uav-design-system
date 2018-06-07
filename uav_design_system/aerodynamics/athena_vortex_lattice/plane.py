59631e4b37ecdeaf27e10da6704f53509f28f111
Vikash Tank <vikashtank@hotmail.co.uk>
2018-06-07 19:38:50 +0100

    tidy up file

--- uav_design_system/aerodynamics/athena_vortex_lattice/plane.py
+++ uav_design_system/aerodynamics/athena_vortex_lattice/plane.py
@@ -7,7 +7,7 @@ from typing import List
 class MainWingError(AttributeError):
     pass
 
-class Plane():
+class Plane:
 
     def __init__(self, name: str):
         self.name = name
@@ -30,15 +30,15 @@ class Plane():
         """
         string at the top of the avl input file with reference values
         """
-        list = [f"{self.name}",
-                "0.0                      Mach",
-                "0     0     0.0          iYsym  iZsym  Zsym",
-                f"{self.reference_area} {self.reference_cord}  {self.reference_span}          "
-                "Sref   Cref   Bref   reference area, chord, span",
-                f"{self.x_ref} {self.y_ref}   {self.z_ref}"
-                "          Xref   Yref   Zref   moment reference location (arb.)",
-                "0.020                    CDoref",
-                "#"
+        list = [f'{self.name}',
+                '0.0                      Mach',
+                '0     0     0.0          iYsym  iZsym  Zsym',
+                f'{self.reference_area} {self.reference_cord}  {self.reference_span}          '
+                'Sref   Cref   Bref   reference area, chord, span',
+                f'{self.x_ref} {self.y_ref}   {self.z_ref}'
+                '          Xref   Yref   Zref   moment reference location (arb.)',
+                '0.020                    CDoref',
+                '#'
                 ]
         return "\n".join(list)
 
@@ -46,15 +46,15 @@ class Plane():
     def _to_avl_string(self):
         plane_string_list = [self._ref_string]
         for index, surface in enumerate(self):
-            plane_string_list.append("#" + "=" * 62)
-            surface_string = surface._to_avl_string(f"surf{index}_")
+            plane_string_list.append('#' + '=' * 62)
+            surface_string = surface._to_avl_string(f'surf{index}_')
             plane_string_list.append(surface_string)
-        return "\n".join(plane_string_list)
+        return '\n'.join(plane_string_list)
 
     def dump_avl_files(self, directory):
         # write .avl file
-        avl_file = os.path.join(directory, f"{self.name}.avl")
-        with open(avl_file, "w") as open_file:
+        avl_file = os.path.join(directory, f'{self.name}.avl')
+        with open(avl_file, 'w') as open_file:
             open_file.write(self._to_avl_string)
 
         aerofoil_files = self._write_aerofoil_files(directory)
@@ -65,14 +65,14 @@ class Plane():
         for j, surface in enumerate(self):
             for i, section in enumerate(surface):
                 try:
-                    file_path = os.path.join(directory, f"surf{j}_sec{i}_af.txt")
+                    file_path = os.path.join(directory, f'surf{j}_sec{i}_af.txt')
                     files.append(self._write_aerofoil_file(file_path, section.aerofoil))
                 except NoAerofoilError:
                     continue
         return files
 
     def _write_aerofoil_file(self, file_path, aerofoil):
-        with open(file_path, "w") as open_file:
+        with open(file_path, 'w') as open_file:
             aerofoil.write(open_file)
         return file_path
 
@@ -93,7 +93,7 @@ class Plane():
         return self.reference_surface.span
 
 
-    def plot_xy(self, subplot = None, marker = "g_"):
+    def plot_xy(self, subplot = None, marker = 'g_'):
 
         if subplot is None:
             subplot = plt.subplot(111)
